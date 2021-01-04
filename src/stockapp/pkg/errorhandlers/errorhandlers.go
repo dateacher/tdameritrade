33d54ceba137917bae3adad51ec2309126879189
dateacher <76852298+dateacher@users.noreply.github.com>
2021-01-03 21:28:42 -0500

    Create errorhandlers.go

---
+++ src/stockapp/pkg/errorhandlers/errorhandlers.go
@@ -0,0 +1,10 @@
+package errorhandlers
+
+import "strings"
+
+func ConfirmBody(inputText string) string {
+	if strings.Contains(inputText, "\"error\":\"Invalid ApiKey\"") {
+		return "Invalid key provided"
+	}
+	return ""
+}
